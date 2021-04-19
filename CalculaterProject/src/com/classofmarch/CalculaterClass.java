8722e6a9033b97335b6924d0f59b3702abfc050c
Ayugeda <ararsaayantu@gmail.com>
2021-04-18 21:32:44 -0700

    commit our first source code

---
+++ CalculaterProject/src/com/classofmarch/CalculaterClass.java
@@ -0,0 +1,11 @@
+package com.classofmarch;
+
+public class CalculaterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+	}
+
+}
